90a583a64aa68d22b4f479157323ece7efc1f706
Cindy <jl7e12@gmail.com>
2018-05-03 23:48:08 +0100

    getIndex test passed

---
+++ airTests/LookUpTest.swift
@@ -0,0 +1,30 @@
+//
+//  LookUpTest.swift
+//  airTests
+//
+//  Created by Jia Liu on 03/05/2018.
+//  Copyright © 2018 Jia Liu. All rights reserved.
+//
+
+import XCTest
+@testable import air
+
+class LookUpTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testGetIndex() {
+        let result = getIndex(find: 12.7 , arr: [11,12,13])
+        XCTAssertEqual(result, 2)
+    }
+    
+    
+}
